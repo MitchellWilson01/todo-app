da82352207c1187aec0c4e78911b83581830bb41
Mitchell Wilson <mitchellwilson01@outlook.com>
2021-05-25 21:23:59 -0500

    restructured firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
+    apiKey: "AIzaSyAOPFXUwdayzAcM4iHFXjzKZ9zMAdYrScQ",
+    authDomain: "auth-development-todo.firebaseapp.com",
+    projectId: "auth-development-todo",
+    storageBucket: "auth-development-todo.appspot.com",
+    messagingSenderId: "644805310054",
+    appId: "1:644805310054:web:bef2261d417cb09f599fa7"
+});
 
 export const auth = app.auth();
 export const firestore = firebase.firestore();
